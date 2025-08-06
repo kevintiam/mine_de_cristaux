d3287d502a40b6f125c1c4c0803091bd932255d9
kevintiam237 <tiamkevin9@gmail.com>
2025-08-06 00:11:29 -0400

    ajout des bonus

--- js/fonction.js
+++ js/fonction.js
@@ -5,6 +5,7 @@ const images = [
   "./img/tile_cristal.png",
   "./img/tile_vide.png",
   "./img/icone_cristal.png",
+  "./img/pasteque.png",
 ];
 
 // initialisation du score et de l'energie
@@ -19,7 +20,7 @@ const creerGrilleJeu = () => {
   // dimensions de la grille
   const hauteur = 15;
   const largeur = 20;
-  // recuperation de l'interface ou la grille sera 
+  // recuperation de l'interface ou la grille sera
   const grilleRocher = document.getElementById("grille");
 
   // On vide la div par mesure de precaution
@@ -28,14 +29,14 @@ const creerGrilleJeu = () => {
   grilleRocher.style.gridTemplateRows = `repeat(${hauteur}, 1fr)`;
 
   // Création d'une grille contenant des pierres (tableau a deux dimensions)
-const grille = Array(hauteur)
-  .fill()
-  .map(() =>
-    Array(largeur).fill({
-      type: "pierre",
-      image: images[1],
-    })
-  );
+  const grille = Array(hauteur)
+    .fill()
+    .map(() =>
+      Array(largeur).fill({
+        type: "pierre",
+        image: images[1],
+      })
+    );
 
   // Positionnement des cristaux
   positionnerCristaux(grille, largeur, hauteur);
@@ -53,6 +54,9 @@ const grille = Array(hauteur)
   };
 
   grille.mineur = grille[centreY][centreX];
+
+  // Positionner les fruits
+  positionnerFruit(grille, largeur, hauteur);
 
   // Remplissage visuel
   for (let y = 0; y < hauteur; y++) {
@@ -72,6 +76,9 @@ const grille = Array(hauteur)
       } else if (cell?.type === "mineur") {
         imgSrc = images[0];
         altText = "mineur";
+      } else if (cell?.type === "fruit") {
+        imgSrc = images[5];
+        altText = "fruit";
       } else {
         imgSrc = images[1];
         altText = "pierre";
@@ -105,6 +112,28 @@ const positionnerCristaux = (grille, largeur, hauteur) => {
       y: y,
       type: "cristal",
       image: images[2],
+    };
+  }
+};
+
+// Fonction pour positionner les fruits pour donner de l'energie
+const positionnerFruit = (grille, largeur, hauteur) => {
+  for (let i = 0; i < 5; i++) {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * largeur);
+      y = Math.floor(Math.random() * hauteur);
+    } while (
+      grille[y][x]?.type === "cristal" ||
+      grille[y][x]?.type === "mineur" ||
+      grille[y][x]?.type === "fruit"
+    );
+
+    grille[y][x] = {
+      x: x,
+      y: y,
+      type: "fruit",
+      image: images[5],
     };
   }
 };
@@ -145,24 +174,26 @@ const deplacerMineur = (direction, grille) => {
 
   // Vérification des limites de la grille
   if (newX >= 0 && newX < largeur && newY >= 0 && newY < hauteur) {
-    // Récupérer la position du mineur dans la grille
+    // Récupérer la position prochaine du mineur dans la grille
     const positionMineur = grille[newY][newX];
 
-    // Vérifier si la position du mineur contient un cristal
+    // Vérifier si la position du mineur contient un cristal ou un fruit
     const contientCristal = positionMineur?.type === "cristal";
+    const contientFruits = positionMineur?.type === "fruit";
 
     // Déterminer si la case cible est une pierre ou un cristal
-    const estPierreOuCristal = positionMineur?.type === "pierre" || positionMineur?.type === "cristal";
+    const estPierreOuCristal =
+      positionMineur?.type === "pierre" || positionMineur?.type === "cristal";
 
-    // Mise à jour de la grille : déplacer le mineur
-    grille[newY][newX] = mineur;
-    grille[mineur.y][mineur.x] = null; // Effacer l'ancienne position du mineur
-
-    // Mise à jour des coordonnées du mineur
+    // coordonnées de l'ancienne position du mineur
     const ancienneX = mineur.x;
     const ancienneY = mineur.y;
+    // coordonnees de la nouvelle position du moineur
     mineur.x = newX;
     mineur.y = newY;
+
+     // Mise à jour de la grille : déplacer le mineur
+    grille[newY][newX] = mineur;
 
     // Mise à jour visuelle
     const anciennePosition = document.querySelector(
@@ -178,8 +209,8 @@ const deplacerMineur = (direction, grille) => {
     }
 
     // Décrémenter l'énergie uniquement si la case cible est une pierre ou un cristal
-    if (estPierreOuCristal) {
-      updateEnergie();
+    if (estPierreOuCristal || contientFruits ) {
+      updateEnergie(contientFruits);
     }
 
     // Mise à jour du score si un cristal est collecté
@@ -197,25 +228,28 @@ const updateScore = (contientCristal) => {
   if (contientCristal) {
     score++;
     document.getElementById("nombreCristaux").textContent = score;
-    document.getElementById("cristalResultat").innerHTML =
-  `Cristaux : ${score} <img src="${images[4]}" alt="cristal" class="img-icon">`;
-    if(score === maxCristaux){
+    document.getElementById(
+      "cristalResultat"
+    ).innerHTML = `Cristaux : ${score} <img src="${images[4]}" alt="cristal" class="img-icon">`;
+    if (score === maxCristaux) {
       endGame();
     }
   }
 };
 
 // Mise à jour de l'énergie
-const updateEnergie = () => {
-  if (energie > 0) {
-    energie--;
-    afficherEnergie();
-    if (energie === 0) {
-      endGame();
+const updateEnergie = (contientFruits) => {
+  if (contientFruits) {
+    // Les fruits donnent de l'énergie
+    energie = Math.min(energie + 2, 40);
+  } else {
+    // Miner coûte de l'énergie
+    if (energie > 0) {
+      energie--;
     }
   }
+  afficherEnergie();
 };
-
 // Affichage de l'énergie
 const afficherEnergie = () => {
   const barreProgress = document.getElementById("progressBarGreen");
@@ -255,4 +289,4 @@ const rejouerPartie = () => {
   return nouvelleGrille;
 };
 
-export { creerGrilleJeu, deplacerMineur,rejouerPartie };
+export { creerGrilleJeu, deplacerMineur, rejouerPartie };
